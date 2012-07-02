edda2740407cd80abe8a22c6a713c0e5237fa52c
Ginny Dellinger <ginny@geekgrade.com>
2012-07-02 15:55:21 -0400

    Initial load

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oatest::Application.config.secret_token = '42911f155066bba67286186c196a9aec8dbb3f6e168bd310a16314ec2dbac2dae5039ee17763c891cec95b97094c8c589bde714ec6356010933e6dc75a10544b'
